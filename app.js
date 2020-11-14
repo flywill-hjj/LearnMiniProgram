a22fd793fad081c7945cbb548dbdcfa6872077d8
hjj14796353396 <1808136161@qq.com>
2020-11-14 20:23:56 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//注册一个小程序示例
 App({
 
   /**
@@ -5,19 +6,6 @@ App({
    */
   onLaunch: function () {
     console.log('小程序初始化完成了:onLaunch');
-    // wx.request({
-    //   url: '',
-    // })
-    setTimeout(()=>{
-      var error = new error();
-    },3000)
-    //异步调用
-    // wx.getUserInfo({
-    //   //数据拿到之后再进行回调的
-    //   success: function (res) {
-    //     console.log(res.userInfo.nickName);
-    //   },
-    // })
   },
 
   /**
@@ -25,8 +13,21 @@ App({
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    console.log('界面显示出来:onShow')
+    //1.判断小程序的进入场景
+    console.log(options)
+    switch (options.scene) {
+      case 1001:
+        break;
+      case 1005:
+        break;
+    }
 
+    //2.获取用户的信息，并且获取到用户信息之后，将用户的信息传递给服务器
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+      }
+    })
   },
 
   /**
@@ -42,5 +43,9 @@ App({
    */
   onError: function (msg) {
     console.log('小程序中发生错误时会执行');
+  },
+  globalData: {
+    name: 'flywill',
+    age: 21
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,46 +1,70 @@
-Page({
-  data: {
-    name: 'flywill',
-    age: 21,
-    students: [{
-        id: 110,
-        name: 'kobe',
-        age: 30
-      },
-      {
-        id: 111,
-        name: 'james',
-        age: 28
-      },
-      {
-        id: 112,
-        name: 'curry',
-        age: 32
-      },
-      {
-        id: 113,
-        name: 'flywill',
-        age: 21
-      }
-    ],
-    counter: 0
-  },
-  handleBtnClick() {
-    //1.错误的做法：界面是不会刷新的
-    // this.data.counter += 1
-    // console.log(this.data.counter);
+//getApp()获取app()产生的示例对象
+// const app = getApp()
+// const name = app.globalData.name
+// const age = app.globalData.age
 
-    //2.this.setData()
-    this.setData({
-      counter: this.data.counter + 1
+//注册一个页面
+//页面也要自己的生命周期
+Page({
+  //---------2初始化数据-------------
+  data: {
+    message: '哈哈哈',
+    list: []
+  },
+
+  //---------1监听页面的生命周期函数-----------
+  //页面被加载处理
+  onLoad() {
+    console.log('onLoad');
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/w6/recommend',
+      success: (res) => {
+        console.log(res);
+        const data = res.data.data.list;
+        this.setData({
+          list: data
+        })
+      }
     })
   },
-  handleSubtraction() {
-    this.setData({
-      counter: this.data.counter - 1
-    })
+  //页面显示出来时
+  onShow() {
+    console.log('onShow');
+  },
+  //页面初次渲染完成时
+  onReady() {
+    console.log('onReady');
+  },
+  //页面隐藏起来时
+  onHide() {
+    console.log('onHide');
+  },
+  onUnload() {
+    console.log('onUnload');
+  },
+
+  //---------3.监听wxml中相关的一些事情-----------
+  handleGetUserInfo(event) {
+    console.log(event);
+  },
+
+
+  //---------4.监听其他事件------------------
+  //监听页面的滚动
+  onPageScroll(obj){
+    // console.log(obj);
+  },
+  //监听我们的页面滚动到底部
+  onReachBottom(){
+    console.log('页面滚动到底部');
+  },
+  onPullDownRefresh(){
+    console.log('下拉刷新');
   }
 })
+
+
+
 
 //编程范式：
 //1.命令式编程：原生操作DOM
