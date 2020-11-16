eef3f026081d85ff3debef51d629410dae64f5ce
hjj14796353396 <1808136161@qq.com>
2020-11-16 18:08:40 +0800

    wxs讲解

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isShow: true,
+    score: 45,
+    movies:['海王','海贼王','海尔兄弟','海盗船']
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleSwitchShow() {
+    this.setData({
+      isShow: !this.data.isShow
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleIncrenment(){
+    this.setData({
+      score: this.data.score + 6
+    })
   }
 })
