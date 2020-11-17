fd970b6b9e79e8eeb184f6fca9f5e84ed2811d9e
hjj14796353396 <1808136161@qq.com>
2020-11-17 21:17:58 +0800

    网络请求及封装

---
+++ service/network.js
@@ -0,0 +1,25 @@
+export default function request(options) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: options.url,
+      method: options.method || 'get',
+      data: options.data || {},
+      success: resolve,
+      fail: reject
+    })
+  })
+
+  // return new Promise((resolve, reject) => {
+  //   wx.request({
+  //     url: options.url,
+  //     method: options.method || 'get',
+  //     data: options.data || {},
+  //     success: function (res) {
+  //       resolve(res)
+  //     },
+  //     fail: function (err) {
+  //       reject(err)
+  //     }
+  //   })
+  // })
+}
