03ef67ce937d0fcd77987dd69a0030212634aff6
hjj14796353396 <1808136161@qq.com>
2020-11-18 15:31:57 +0800

    登录流程和演练

--- app.js
+++ app.js
@@ -1,30 +1,74 @@
-App({
+const TOKEN = 'token';
 
+App({
+  //对象：小程序关闭掉
+  globalData: {
+    token: ''
+  },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
-  },
+    //1.先从缓存中取出我们的token
+    const token = wx.getStorageSync(TOKEN);
 
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
-  onShow: function (options) {
-    
-  },
+    //2.判断token是否有值
+    if (token && token.length != 0) { //已经有token,验证token是否过期
+      this.check_token(token) //验证token是否过期
+    } else { //没有token，进行登录操作
+      this.login()
+    }
 
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
-  onHide: function () {
-    
-  },
+    //登录操作
 
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError: function (msg) {
-    
+  },
+  check_token(token) {
+    console.log('执行了验证token的操作');
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: 'POST',
+      header: {
+        token
+      },
+      success: (res) => {
+        if (!res.data.errCode) {
+          console.log('token有效');
+          this.globalData.token = token
+        } else {
+          this.login();
+        }
+      },
+      fail: (err) => {
+        console.log(err);
+      }
+    })
+  },
+  login() {
+    console.log('验证登录的操作');
+    wx.login({
+      //code只有只有5分钟的有效期
+      success: (res) => {
+        //1.获取code
+        const code = res.code
+
+        //2.将code发送给我们的服务器
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'POST',
+          data: {
+            code
+          },
+          success: (res) => {
+            console.log(res);
+            //1取出token
+            const token = res.data.token
+            //2.将token保持在globaData中
+            this.globalData.token = token;
+            //3.进行本地存储
+            wx.setStorageSync(TOKEN, token)
+          }
+        })
+      }
+    })
   }
 })
