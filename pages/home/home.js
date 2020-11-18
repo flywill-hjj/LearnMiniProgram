682165ff439f2734746072d7833c3ba0c45ae47e
hjj14796353396 <1808136161@qq.com>
2020-11-19 01:17:02 +0800

    项目练习-首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,40 +1,132 @@
 // pages/home/home.js
+import {
+  getMuliData,
+  getGoodsData
+} from '../../service/home.js'
+
+const TOP_DISTANCE = 1000;
+const types = ['pop', 'new', 'sell']
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    counter: 0,
-    isShow: true
+    banners: [],
+    recommends: [],
+    titles: ['流行', '新款', '精选'],
+    goods: {
+      pop: {
+        page: 0,
+        list: []
+      },
+      new: {
+        page: 0,
+        list: []
+      },
+      sell: {
+        page: 0,
+        list: []
+      }
+    },
+    currentType: 'pop',
+    showBackTop: false,
+    isTabFixed: false,
+    tabScrollTop: 0
   },
-  handleChangeShow() {
-    this.setData({
-      isShow: !this.data.isShow
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //1.请求轮播图以及推荐数据
+    this._getMultidata();
+
+    //2.请求商品数据
+    this._getGoodsData('pop');
+    this._getGoodsData('new');
+    this._getGoodsData('sell');
+  },
+  //onShow:页面显示出来的时回调的函数
+  //页面显示是否意味着所有的图片都加载完成
+  onShow() {
+
+  },
+
+  //------------------------网络请求函数-----------------
+  _getMultidata() {
+    getMuliData().then(res => {
+      //取出轮播图和推荐的数据
+      const banners = res.data.data.banner.list;
+      const recommends = res.data.data.recommend.list
+
+      //将banners和recommends放到data中
+      this.setData({
+        banners,
+        recommends
+      })
     })
   },
-  increment(event) {
-    console.log(event.detail.name);
-    console.log(event.detail.age);
+  _getGoodsData(type) {
+    //1.获取页码
+    const page = this.data.goods[type].page + 1;
 
-    this.setData({
-      counter: this.data.counter + 1
+    //2.发送网络请求
+    getGoodsData(type, page).then(res => {
+      //2.1.取出数据
+      const list = res.data.data.list;
+
+      //2.2将数据设置到对应type的list里面
+      const oldList = this.data.goods[type].list;
+      oldList.push(...list);
+
+      //2.3将数据设置到data中的goods中
+      const typeKey = `goods.${type}.list`;
+      const pageKey = `goods.${type}.page`;
+      this.setData({
+        [typeKey]: oldList,
+        [pageKey]: page
+      })
     })
   },
-  tabControlClick(event) {
-    // console.log(event);
+
+  //------------------事件监听-----------------
+  handleTabClick(event) {
+    //取出index
+    const index = event.detail.index
+
+    // 设置currentType
+    this.setData({
+      currentType: types[index]
+    })
   },
-  handleIncrementCpn() {
-    //最终目的:修改my-sel中的counter
-    //1.获取组件对象
-    const my_sel = this.selectComponent("#sel-id");
+  handleImageLoad() {
+    wx.createSelectorQuery().select('#tab-control').boundingClientRect(rect => {
+      this.data.tabScrollTop = rect.top
+    }).exec()
+  },
+  onReachBottom() {
+    //上拉加载更多
+    this._getGoodsData(this.data.currentType)
+  },
+  onPageScroll(options) {
+    //1.取出scrollTop
+    const scrollTop = options.scrollTop
 
-    //2.通过setData修改组件中的数据(不合理)
-    // my_sel.setData({
-    //   counter : my_sel.data.counter + 1
-    // })
+    //2.修改showbackTop属性
+    //官方：不要在滚动的函数回调中频繁的调用setData
+    const flag1 = scrollTop >= TOP_DISTANCE
+    if (flag1 != this.data.showBackTop) {
+      this.setData({
+        showBackTop: flag1
+      })
+    }
 
-    //3.通过方法对数据进行修改
-    my_sel.incrementCounter(1)
+    //3.修改isTabFixed属性
+    const flag2 = scrollTop >= this.data.tabScrollTop;
+    if (flag2 != this.data.isTabFixed) {
+      this.setData({
+        isTabFixed: flag2
+      })
+    }
   }
 })
